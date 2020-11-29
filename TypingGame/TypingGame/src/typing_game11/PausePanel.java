4a3c8945ade17fb1d68ae0746ba52b5c72b53d5c
JGeun <pjk8926@gmail.com>
2020-11-30 02:33:40 +0900

    add SettingPanel and remove error

---
+++ TypingGame/TypingGame/src/typing_game11/PausePanel.java
@@ -0,0 +1,41 @@
+package typing_game11;
+
+import javax.swing.*;
+
+import typing_game11.GamePanel.WordThread;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class PausePanel extends JPanel{
+	private Image screenImage;
+	private Graphics screenGraphic;
+	private Image background;
+	private JButton btn = new JButton("click");
+	
+	public PausePanel(Container contentPane, GamePanel gamePanel, WordThread wordThread) {
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		
+		setLayout(null);
+		PauseMenuPanel menuPanel = new PauseMenuPanel(contentPane, gamePanel, this, wordThread);
+		repaint();
+		menuPanel.setLocation(Main.SCREEN_WIDTH/2-240, 70);
+		add(menuPanel);
+		
+	}
+	
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		updateScreen(g);
+	}
+	
+	public void updateScreen(Graphics g) {
+		screenGraphic.drawImage(Main.pauseBackground, 0, 0, null);
+		paintComponents(screenGraphic);
+		g.drawImage(screenImage, 0, 0, this);
+		revalidate();
+		repaint();
+	}
+}
